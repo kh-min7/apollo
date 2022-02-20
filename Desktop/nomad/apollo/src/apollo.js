f337c803f493ef6eccace625f8152b9813573b84
MIN <rudgh2879@naver.com>
2022-02-20 20:42:47 +0900

    .

--- Desktop/nomad/apollo/src/apollo.js
+++ Desktop/nomad/apollo/src/apollo.js
@@ -1,8 +1,23 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://movieql2.vercel.app/",
   cache: new InMemoryCache(),
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.modify({
+          id: `Movie:${id}`,
+          fields: {
+            isLiked: (isLiked) => !isLiked, // 현재 isLiked 값을 받아서 true-false 토글
+          },
+        });
+      },
+    },
+  },
 });
 
 export default client;
